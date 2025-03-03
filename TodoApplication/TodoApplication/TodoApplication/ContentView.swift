25939271718d3c8243b0888b8d55faaac8b14219
DanielKonjarski <daniel.konjarski@georgebrown.ca>
2025-03-02 23:27:49 -0500

    Project for Mobile Application Creation

---
+++ TodoApplication/TodoApplication/TodoApplication/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  TodoApplication
+//
+//  Created by Daniel Konjarski & Carl Trinidad
+//
+import SwiftUI
+
+@main
+struct ContentView: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                WelcomeView()
+            }
+        }
+    }
+}
